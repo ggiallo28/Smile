45bbb21a8a3af9332f09bfd3f60f19fce989a7e1
Giallo <th3g91@gmail.com>
2016-10-05 01:12:05 +0200

    some fix

--- Matlab/main.m
+++ Matlab/main.m
@@ -7,12 +7,12 @@ pivot2pivot = 28.5;         %cm
 lengthHead = 20;            %cm % vertical 2*radius
 widthHead = 18;             %cm % horizontal 2*radius
 %% Parameters
-angleRightMirror = 73.7;    %deg
+angleRightMirror = 43.7;    %deg
 angleLeftMirror = 106.3;    %deg
 distanceCamera = 200;       %cm
 fovHCamera = 83;            %deg
-headPosx = 0.5*pivot2pivot; %cm % x0,y0 ellipse centre coordinates
-headPosy = 40;              %cm
+headPosx = 6+0.5*pivot2pivot; %cm % x0,y0 ellipse centre coordinates
+headPosy = 36;              %cm
 %% Logic
 lengthPivotMirrors = lengthMirrors+mirror2Pivot;
 angleBetweenMirror = abs(angleRightMirror-angleLeftMirror);
@@ -48,55 +48,34 @@ angleHead = rad2deg(atan2((-headPosy-mirrorsCenter(2)),(headPosx-mirrorsCenter(1
 if angleHead<0
     angleHead = -angleHead;
 end
-el_x = headPosx+0.5*widthHead*cos(-pi:0.01:pi);
-el_y = -headPosy+0.5*lengthHead*sin(-pi:0.01:pi);
-h2m(1) = abs(angleLeftMirror-angleHead); % Angle 2 Left
-h2m(2) = abs(angleRightMirror-angleHead); % Angle 2 Right 
-%% Compute Rotations, Definisco sfasamenti in angoli tra le teste
-isLeft = true; l = 2; r = 1; v = zeros(1,floor(N)-1);
-v(1) = -2*h2m(1);
-v(2) = 2*h2m(2);
-for i=3:floor(N)-1 
-    if(isLeft)
-        v(i) = v(i-2) - 2*h2m(l);
-        if l==1
-            l = 2;
-        else
-            l = 1;
-        end
-    else
-        v(i) = v(i-2) + 2*h2m(r);
-        if r==1
-            r = 2;
-        else
-            r = 1;
-        end
-    end
-    isLeft = ~isLeft;
-end
+[el_x, el_y] = genHead(headPosx, -headPosy, widthHead, lengthHead);
+v = genMirroring(angleLeftMirror, angleRightMirror, angleHead, N);
 el_X = zeros(size(v,2),size(el_x,2));
 el_Y = zeros(size(v,2),size(el_y,2));
 for i=1:size(v,2);
-    [el_X(i,:),el_Y(i,:)] = rotate(el_x,el_y,mirrorsCenter(1),mirrorsCenter(2),v(i));
+    el_x_flip = el_x;
+%     if v(3,i) == -1
+%         el_x_flip = fliplr(el_x_flip); %% Verificare
+%     elseif v(3,i) == 1
+%         el_x_flip = fliprl(el_x_flip);
+%     end
+    [el_X(i,:),el_Y(i,:)] = rotate(el_x_flip,el_y,mirrorsCenter(1),mirrorsCenter(2),v(1,i));
 end
-%% Compute projections
+
 %% Plot Inverti le Y. plot([x1 x2], [y1 y2])
-figure,hold on,axis([-200 200 -200 200])
+figure,hold on,axis([-distanceCamera distanceCamera -distanceCamera distanceCamera])
 plot([0 pivot2pivot], [0 0],'b');
 plot([0 inPointLeft(1)], [0 -inPointLeft(2)],'b');
 plot([pivot2pivot inPointRight(1)], [0 -inPointRight(2)],'b');
 plot([inPointRight(1) exPointRight(1)], [-inPointRight(2) -exPointRight(2)],'b');
+plot([inPointLeft(1) inPointRight(1)], [-inPointLeft(2) -inPointRight(2)],'b');
 plot([inPointLeft(1) exPointLeft(1)], [-inPointLeft(2) -exPointLeft(2)],'b');
+
 scatter(mirrorsCenter(1),mirrorsCenter(2));
 scatter(cameraCenter(1),cameraCenter(2));
 plot(el_x,el_y,'b')
 plot([min(el_x) cameraCenter(1)], [median(el_y) cameraCenter(2)],'b');
 plot([max(el_x) cameraCenter(1)], [median(el_y) cameraCenter(2)],'b');
-
-
-spl = pchip(el_X(1,:),el_Y(1,:));
-splder = fnder(spl);
-d = fnval(splder,[415,203,365]);
 
 for i=1:size(v,2);
     plot(el_X(i,:),el_Y(i,:));
@@ -105,8 +84,22 @@ for i=1:size(v,2);
     plot([p2(1) cameraCenter(1)], [p2(2) cameraCenter(2)]);
 end
 
-
-
+%% Disegnare elemento centrale
+mul = 2;
+for i=1:size(v,2);
+    if v(2,i) == 1
+        [xx,yy] = rotate(inPointLeft(1), -inPointLeft(2),mirrorsCenter(1),mirrorsCenter(2),mul*angleBetweenMirror);
+        plot([inPointRight(1) xx],[-inPointRight(2) yy]);
+    else
+        [xx,yy] = rotate(inPointRight(1), -inPointRight(2),mirrorsCenter(1),mirrorsCenter(2),-mul*angleBetweenMirror);
+        plot([ inPointLeft(1) xx],[-inPointLeft(2) yy]);
+    end
+    if mod(i,2) == 0 && i> 1
+        mul = mul+2;
+    end
+end
+%% TODO fare flip e disegnare figura al centro
+%% TODO calcolare indice di copertura
 
 
 
