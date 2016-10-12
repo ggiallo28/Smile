fb69895be945dc15f0ee7c8e60a3d2bceaca91e0
Giallo <th3g91@gmail.com>
2016-10-12 23:25:01 +0200

    Some fix

--- Calibration/100MSDCF/putCenters.m
+++ Calibration/100MSDCF/putCenters.m
@@ -8,14 +8,24 @@ function obj = putCenters(obj, xOffset, yOffset, centers, index, img_dim)
         x(i) = v(1);
         y(i) = v(2);
     end 
-    th = floor(img_dim/10);
+    th = floor(img_dim/10);  
+    v = [];
+    for k=floor(img_dim/10):2*floor(img_dim/10):img_dim
+        idx = find(abs(y-k)<th);
+        v = [v;size(idx,2)];
+    end
     j = 1;
     for k=floor(img_dim/10):2*floor(img_dim/10):img_dim
         idx = find(abs(y-k)<th);
+        if size(idx,2)>mode(v)
+            idx = idx(1:mode(v));
+        end
         for i=1:size(idx,2)
             obj.chess(index).center_x(j,i) = xOffset+x(idx(i));
             obj.chess(index).center_y(j,i) = yOffset+y(idx(i));
         end
+        x(idx) = [];
+        y(idx) = [];
         j = j+1;
     end
 end
